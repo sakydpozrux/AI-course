7e2196b893e9754ffad9266a1a27f4fea992d7b0
Szymon Koper <sakydpozrux@gmail.com>
2014-04-01 18:19:43 +0200

    Beginning of implementing game state using lists

--- 2-Checkers-heuristic/evaluateposition.java
+++ 2-Checkers-heuristic/evaluateposition.java
@@ -1,9 +1,35 @@
+import java.util.List;
+
 package checkers; // This package is required - don't remove it
+
+
+public class Checker
+{
+  private int _x;
+  private int _y;
+  private boolean _isKing;
+  
+  public Checker(int x, int y, boolean isKing)
+  {
+    _x = x;
+    _y = y;
+    _isKing = isKing;
+  }
+  
+  static public int getX() { return _x; }
+  static public int getY() { return _y; }
+}
+
 public class EvaluatePosition // This class is required - don't remove it
 {
 	static private final int WIN=Integer.MAX_VALUE/2;
 	static private final int LOSE=Integer.MIN_VALUE/2;
 	static private boolean _color; // This field is required - don't remove it
+	
+	private AIBoard board;
+	private List<Checker> friendlyCheckers = new LinkedList<Checker>();
+	private List<Checker> enemyCheckers = new LinkedList<Checker>();
+	
 	static public void changeColor(boolean color) // This method is required - don't remove it
 	{
 		_color=color;
@@ -13,15 +39,15 @@ public class EvaluatePosition // This class is required - don't remove it
 		return _color;
 	}
 
-  static private int evalPos_simpleCheckersCount(AIBoard board, int myRating, int opponentsRating) // Modified example method
+  static private int evalPos_simpleCheckersCount() // Modified example method
   { // TODO 1 - FIX KINGS COUNTING IN evaluatePosition(AIBoard board). Maybe use list of references to checkers?
     return myRating-opponentsRating;
   }
-  static private int evalPos_weightedCheckersCount(AIBoard board, int myRating, int opponentsRating)
+  static private int evalPos_weightedCheckersCount()
   {
     // TODO
   }
-  static private int evalPos_threeAreas
+  static private int evalPos_threeAreas()
   {
     // TODO
   }
@@ -35,6 +61,7 @@ public class EvaluatePosition // This class is required - don't remove it
   
 	static public int evaluatePosition(AIBoard board) // This method is required and it is the major heuristic method - type your code here
 	{
+   
 	  int myRating = 0;
 		int opponentsRating = 0;
 		int size = board.getSize();
