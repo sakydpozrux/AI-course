98445e530f2c7fc9be07655e1a0e6f51de06d70b
Szymon Koper <sakydpozrux@gmail.com>
2014-04-01 18:36:31 +0200

    Small progress in implementing list idea

--- 2-Checkers-heuristic/evaluateposition.java
+++ 2-Checkers-heuristic/evaluateposition.java
@@ -16,19 +16,38 @@ public class Checker
     _isKing = isKing;
   }
   
-  static public int getX() { return _x; }
-  static public int getY() { return _y; }
+  static public int     getX()   { return _x; }
+  static public int     getY()   { return _y; }
+  static public boolean ifKing() { return _isKing; }
 }
 
 public class EvaluatePosition // This class is required - don't remove it
 {
-	static private final int WIN=Integer.MAX_VALUE/2;
-	static private final int LOSE=Integer.MIN_VALUE/2;
+	static private final int WIN  = Integer.MAX_VALUE / 2;
+	static private final int LOSE = Integer.MIN_VALUE / 2;
 	static private boolean _color; // This field is required - don't remove it
 	
-	private AIBoard board;
-	private List<Checker> friendlyCheckers = new LinkedList<Checker>();
-	private List<Checker> enemyCheckers = new LinkedList<Checker>();
+	static private final int  KING_VAL = 5;
+	static private final int PIECE_VAL = 1;
+	
+	private AIBoard _board;
+	private List<Checker> _friendlyCheckers = new LinkedList<Checker>();
+	private List<Checker> _opponentCheckers = new LinkedList<Checker>();
+	
+	static private int totalFriendly()
+	{
+	  return _friendlyCheckers.size();
+	}
+	
+	static private int totalOpponent()
+	{
+	  return _opponentCheckers.size();
+	}
+	
+	static private int totalBoth()
+	{
+	  return totalFriendly() + totalOpponent();
+	}
 	
 	static public void changeColor(boolean color) // This method is required - don't remove it
 	{
@@ -39,9 +58,9 @@ public class EvaluatePosition // This class is required - don't remove it
 		return _color;
 	}
 
-  static private int evalPos_simpleCheckersCount() // Modified example method
-  { // TODO 1 - FIX KINGS COUNTING IN evaluatePosition(AIBoard board). Maybe use list of references to checkers?
-    return myRating-opponentsRating;
+  static private int evalPos_simpleCheckersCount()
+  {
+    return totalFriendly() - totalOpponent();
   }
   static private int evalPos_weightedCheckersCount()
   {
@@ -53,7 +72,7 @@ public class EvaluatePosition // This class is required - don't remove it
   }
   
   
-  static private int evalPos_bestMethodAIBoard board, int myRating, int opponentsRating)
+  static private int evalPos_bestMethod()
   {
     // TODO
     // In function below you should use something like "switch evalPos_chooseMethod(board) {...} "  
@@ -61,24 +80,16 @@ public class EvaluatePosition // This class is required - don't remove it
   
 	static public int evaluatePosition(AIBoard board) // This method is required and it is the major heuristic method - type your code here
 	{
-   
-	  int myRating = 0;
-		int opponentsRating = 0;
+	  _board = board;
 		int size = board.getSize();
 		
 		for (int i = 0; i < size; i++)
 			for (int j = (i + 1) % 2; j < size; j += 2)
 				if (!board._board[i][j].empty) // field is not empty
 					if (board._board[i][j].white == getColor()) // this is my piece
-					{
-						if (board._board[i][j].king) myRating += 5; // this is my king
-						else myRating += 1; // this is my piece
-					}
-					else
-					{
-						if (board._board[i][j].king) opponentsRating += 5; // This is opponent's king
-						else opponentsRating += 1;
-					}
+					  _friendlyCheckers.add(new Checker(i, j, board._board[i][j].king));
+					else // this is enemy piece
+						_opponentCheckers.add(new Checker(i, j, board._board[i][j].king));
 		
 		if (myRating == 0) return LOSE; 
 		else if (opponentsRating == 0) return WIN;
