0dc7e6635b2a0b22b87dcfd5e8d7e8a1e8d74cff
Szymon Koper <sakydpozrux@gmail.com>
2014-04-01 15:00:25 +0200

    Code structure

--- 2-Checkers-heuristic/evaluateposition.java
+++ 2-Checkers-heuristic/evaluateposition.java
@@ -13,12 +13,21 @@ public class EvaluatePosition // This class is required - don't remove it
 		return _color;
 	}
 
-  static private int evalPos_simpleCount(AIBoard board)
+  static private int evalPos_simpleCheckersCount(AIBoard board, int myRating, int opponentsRating) // Modified example method
+  { // TODO 1 - FIX KINGS COUNTING IN evaluatePosition(AIBoard board). Maybe use list of references to checkers?
+    return myRating-opponentsRating;
+  }
+  static private int evalPos_weightedCheckersCount(AIBoard board, int myRating, int opponentsRating)
+  {
+    // TODO
+  }
+  static private int evalPos_threeAreas
   {
     // TODO
   }
   
-  static private int evalPos_chooseMethod(AIBoard board)
+  
+  static private int evalPos_bestMethodAIBoard board, int myRating, int opponentsRating)
   {
     // TODO
     // In function below you should use something like "switch evalPos_chooseMethod(board) {...} "  
@@ -26,32 +35,27 @@ public class EvaluatePosition // This class is required - don't remove it
   
 	static public int evaluatePosition(AIBoard board) // This method is required and it is the major heuristic method - type your code here
 	{
-		int myRating=0;
-		int opponentsRating=0;
-		int size=board.getSize();
-		for (int i=0;i<size;i++)
-		{
-			for (int j=(i+1)%2;j<size;j+=2)
-			{
+	  int myRating = 0;
+		int opponentsRating = 0;
+		int size = board.getSize();
+		
+		for (int i = 0; i < size; i++)
+			for (int j = (i + 1) % 2; j < size; j += 2)
 				if (!board._board[i][j].empty) // field is not empty
-				{
-					if (board._board[i][j].white==getColor()) // this is my piece
+					if (board._board[i][j].white == getColor()) // this is my piece
 					{
-						if (board._board[i][j].king) myRating+=5; // this is my king
-						else myRating+=1; // this is my piece
+						if (board._board[i][j].king) myRating += 5; // this is my king
+						else myRating += 1; // this is my piece
 					}
 					else
 					{
-						if (board._board[i][j].king) opponentsRating+=5; // This is opponent's king
-						else opponentsRating+=1;
+						if (board._board[i][j].king) opponentsRating += 5; // This is opponent's king
+						else opponentsRating += 1;
 					}
-				}
-			}
-		}
-		//Judge.updateLog("Type your message here, you will see it in the log window\n");
-		if (myRating==0) return LOSE; 
-		else if (opponentsRating==0) return WIN; 
-		else return myRating-opponentsRating;
+		
+		if (myRating == 0) return LOSE; 
+		else if (opponentsRating == 0) return WIN;
+		else return evalPos_bestMethod(board, myRating, opponentsRating);
 	}
 }
 
