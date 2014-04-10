67e13a54b6876dcc7fff41ea09743bca05343c4d
Szymon Koper <sakydpozrux@gmail.com>
2014-04-10 21:47:12 +0200

    Progress in strategy choosing

--- 2-Checkers-heuristic/evaluateposition.java
+++ 2-Checkers-heuristic/evaluateposition.java
@@ -16,15 +16,17 @@ public class EvaluatePosition // This class is required - don't remove it
 		return _color;
 	}
 	
+	static int rotateCoordinates(int n, int size)
+  {
+    return size - 1 - n;
+  }
 	
 	
-	static private enum GamePart
-	{
-	  BEGINNING,
-	  MIDGAME,
-	  END_LOSING,
-	  END_WINNING
-	}
+
+	static private final int BEGINNING = 111;
+	static private final int MIDGAME = 222;
+	static private final int END_YELLOW_WINNING = 333;
+	static private final int END_RED_WINNING = 444;
 
   static private boolean YELLOW = true;
   static private boolean RED = false;
@@ -32,6 +34,14 @@ public class EvaluatePosition // This class is required - don't remove it
   
   static private int piece_val = 10;
   static private int king_val = 19;
+  static private int guard_bonus = 5;
+  
+  static private int zone_not_border_bonus = 2;
+  static private int zone_center_bonus = 1; // note that it's inside zone_not_border, so in fact it's always +3
+  static private int zone_strategic_field_bonus = 2;
+  
+  static private int lower_center_coordinate;
+  static private int upper_center_coordinate;
 
 	
   static int rate(int ratingYellow, int ratingRed) //int ratingYellow, int ratingRed)
@@ -55,22 +65,6 @@ public class EvaluatePosition // This class is required - don't remove it
   }
 
 
-  static int bestEvaluateStrategy(int[][] yellowPieces, int yellowCount, int[][] redPieces, int redCount)
-  {
-    int totalCount = yellowCount + redCount;
-    
-    GamePart part;
-    //if (totalCount >= 16) part = BEGINNING;
-    //else if (totalCount >= 8) part = MIDGAME;
-    //else if (totalCount 
-    
-    int ratingYellow = evaluateSimpleWeights(yellowPieces, yellowCount);
-    int ratingRed = evaluateSimpleWeights(redPieces, redCount);
-
-    return rate(ratingYellow, ratingRed);
-  }
-
-
 	static int evaluateSimpleWeights(int[][] pieces, int count)
 	{
 		int rating = 0;
@@ -78,16 +72,109 @@ public class EvaluatePosition // This class is required - don't remove it
 		for (int i = 0; i < count; ++i)
 		{
 		  if (pieces[i][2] == KING) rating += king_val;
-		  else rating += piece_val;
+		  rating += piece_val;
 		}
 
 		return rating;
   }
   
-  static int rotateCoordinates(int n, int size)
-  {
-    return size - 1 - n;
+  static int evaluateMidGame(int[][] pieces, int count)
+  { //TODO
+		int rating = 0;
+		
+		int x;
+		int y;
+		for (int i = 0; i < count; ++i)
+		{
+		  x = pieces[i][0];
+		  y = pieces[i][1];
+		  if (x)
+		  
+		  if (pieces[i][2] == KING) rating += king_val;
+		  rating += piece_val;
+		}
+
+		return rating;
   }
+  
+  static int evaluateWinning(int[][] pieces, int count)
+  { //TODO
+		int rating = 0;
+		
+		for (int i = 0; i < count; ++i)
+		{
+		  if (pieces[i][2] == KING) rating += king_val;
+		  rating += piece_val;
+		}
+
+		return rating;
+  }
+  
+  static int evaluateLosing(int[][] pieces, int count)
+  { //TODO
+		int rating = 0;
+		
+		for (int i = 0; i < count; ++i)
+		{
+		  if (pieces[i][2] == KING) rating += king_val;
+		  rating += piece_val;
+		}
+
+		return rating;
+  }
+
+
+  static int bestEvaluateStrategy(int[][] yellowPieces, int yellowCount, int[][] redPieces, int redCount)
+  {
+    int totalCount = yellowCount + redCount;
+    
+    
+    
+    int part;
+    if (totalCount >= 16)
+      part = BEGINNING;
+    else if (totalCount >= 8)
+      part = MIDGAME;
+    else if (yellowCount > redCount)
+      part = END_YELLOW_WINNING;
+    else if (redCount > yellowCount)
+      part = END_RED_WINNING;
+    else
+      part = MIDGAME; // play as in midgame
+      
+    int ratingYellow, ratingRed;
+      
+    switch (part)
+    {
+      case BEGINNING:
+        ratingYellow = evaluateSimpleWeights(yellowPieces, yellowCount);
+        ratingRed    = evaluateSimpleWeights(redPieces, redCount);
+        break;
+      
+      case MIDGAME:
+        ratingYellow = evaluateMidGame(yellowPieces, yellowCount);
+        ratingRed    = evaluateMidGame(redPieces, redCount);
+        break;
+      
+      case END_YELLOW_WINNING:
+        ratingYellow = evaluateWinning(yellowPieces, yellowCount);
+        ratingRed    = evaluateLosing(redPieces, redCount);
+        break;
+      
+      default: //case END_RED_WINNING:
+        ratingYellow = evaluateLosing(yellowPieces, yellowCount);
+        ratingRed    = evaluateWinning(redPieces, redCount);
+        break;
+    }
+    
+    
+
+
+    return rate(ratingYellow, ratingRed);
+  }
+
+
+
 
 	
 	static public int evaluatePosition(AIBoard board) // This method is required and it is the major heuristic method - type your code here
@@ -99,6 +186,9 @@ public class EvaluatePosition // This class is required - don't remove it
     
     int yellowCount = 0;
     int redCount = 0;
+    
+    lower_center_coordinate = 1 * size / 4;
+    upper_center_coordinate = 3 * size / 4;
 		
 		for (int i = 0; i < size; i++)
 			for (int j = (i + 1) % 2; j < size; j += 2)
