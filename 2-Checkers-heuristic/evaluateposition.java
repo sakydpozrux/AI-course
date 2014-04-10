3fa4922d47006022887253695a120bdc151978df
Szymon Koper <sakydpozrux@gmail.com>
2014-04-10 10:11:09 +0200

    Structure change

--- 2-Checkers-heuristic/evaluateposition.java
+++ 2-Checkers-heuristic/evaluateposition.java
@@ -12,33 +12,73 @@ public class EvaluatePosition // This class is required - don't remove it
 	{
 		return _color;
 	}
+	
+	
+	
+	
+
+  static private boolean YELLOW = true;
+  static private boolean RED = false;
+
+	
+	
+	
+
+	
+	
 	static public int evaluatePosition(AIBoard board) // This method is required and it is the major heuristic method - type your code here
 	{
-		int myRating=0;
-		int opponentsRating=0;
-		int size=board.getSize();
-		for (int i=0;i<size;i++)
+		int ratingYellow = 0; //upper
+		int ratingRed = 0; //lower
+		
+		int size = board.getSize();
+		
+		for (int i = 0; i < size; i++)
 		{
-			for (int j=(i+1)%2;j<size;j+=2)
+			for (int j = (i + 1) % 2; j < size; j += 2)
 			{
 				if (!board._board[i][j].empty) // field is not empty
 				{
-					if (board._board[i][j].white==getColor()) // this is my piece
+				  if (board._board[i][j].white) // this is upper, yellow piece
 					{
-						if (board._board[i][j].king) myRating+=5; // this is my king
-						else myRating+=1; // this is my piece
+					  if (board._board[i][j].king) ratingYellow += 5; // this is upper, yellow king
+					  else ratingYellow += 1; // this is upper, yellow piece
 					}
 					else
-					{
-						if (board._board[i][j].king) opponentsRating+=5; // This is opponent's king
-						else opponentsRating+=1;
+					{	
+					  if (board._board[i][j].king) ratingRed += 5; // This is opponent's king
+					  else ratingRed += 1;
 					}
 				}
 			}
 		}
-		//Judge.updateLog("Type your message here, you will see it in the log window\n");
+		
+		
+		int myRating, opponentsRating;
+		if (getColor())
+		{
+		  myRating = ratingYellow;
+		  opponentsRating = ratingRed;
+		}
+		else
+		{
+		  myRating = ratingRed;
+		  opponentsRating = ratingYellow;
+		}
+		
+		
+		
+		
+		
+		
+		
 		if (myRating==0) return LOSE; 
 		else if (opponentsRating==0) return WIN; 
+		
+		
+		
+		
+		
 		else return myRating-opponentsRating;
 	}
 }
