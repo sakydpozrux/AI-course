0e91f23f61c97bc8cd09afd4132b8a0dc83754c1
Szymon Koper <sakydpozrux@gmail.com>
2014-04-10 12:09:11 +0200

    This version uses arrays

--- 2-Checkers-heuristic/evaluateposition.java
+++ 2-Checkers-heuristic/evaluateposition.java
@@ -1,4 +1,7 @@
 package checkers; // This package is required - don't remove it
+
+import java.util.*;
+
 public class EvaluatePosition // This class is required - don't remove it
 {
 	static private final int WIN=Integer.MAX_VALUE/2;
@@ -21,39 +24,8 @@ public class EvaluatePosition // This class is required - don't remove it
   static private boolean RED = false;
 
 	
-	
-	
-
-	
-	
-	static public int evaluatePosition(AIBoard board) // This method is required and it is the major heuristic method - type your code here
-	{
-		int ratingYellow = 0; //upper
-		int ratingRed = 0; //lower
-		
-		int size = board.getSize();
-		
-		for (int i = 0; i < size; i++)
-		{
-			for (int j = (i + 1) % 2; j < size; j += 2)
-			{
-				if (!board._board[i][j].empty) // field is not empty
-				{
-				  if (board._board[i][j].white) // this is upper, yellow piece
-					{
-					  if (board._board[i][j].king) ratingYellow += 5; // this is upper, yellow king
-					  else ratingYellow += 1; // this is upper, yellow piece
-					}
-					else
-					{	
-					  if (board._board[i][j].king) ratingRed += 5; // This is opponent's king
-					  else ratingRed += 1;
-					}
-				}
-			}
-		}
-		
-		
+  /*static int rate(int ratingYellow, int ratingRed)
+  {
 		int myRating, opponentsRating;
 		if (getColor())
 		{
@@ -66,20 +38,81 @@ public class EvaluatePosition // This class is required - don't remove it
 		  opponentsRating = ratingYellow;
 		}
 		
-		
-		
-		
-		
-		
-		
-		if (myRating==0) return LOSE; 
-		else if (opponentsRating==0) return WIN; 
-		
-		
-		
-		
-		
+		if (myRating == 0) return LOSE; 
+		else if (opponentsRating == 0) return WIN; 
 		else return myRating-opponentsRating;
+  }*/
+
+
+	/*static int simpleWeights(AIboard board)
+	{
+		int ratingYellow = 0; //upper
+		int ratingRed = 0; //lower
+		
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = (i + 1) % 2; j < size; j += 2)
+			{
+				if (!board._board[i][j].empty) // field is not empty
+				{
+				  if (board._board[i][j].white) // this is upper, yellow piece
+					{
+					  if (board._board[i][j].king) ratingYellow += 25; // this is upper, yellow king
+					  else ratingYellow += 10; // this is upper, yellow piece
+					}
+					else
+					{	
+					  if (board._board[i][j].king) ratingRed += 25; // This is opponent's king
+					  else ratingRed += 10;
+					}
+				}
+			}
+		}
+		
+		return rate(ratingYellow, ratingRed);
+  }	*/
+
+	
+	static public int evaluatePosition(AIBoard board) // This method is required and it is the major heuristic method - type your code here
+	{
+		int size = board.getSize();
+		
+    int yellowPieces[][] = new int [size*size][3]; // (i, j, king?)
+    int redPieces[][] = new int [size*size][3];
+    
+    int yellowCount = 0;
+    int redCount = 0;
+		
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = (i + 1) % 2; j < size; j += 2)
+			{
+				if (!board._board[i][j].empty) // field is not empty
+				{
+				  if (board._board[i][j].white) // this is upper, yellow piece
+					{
+					  yellowCount += 1;
+					  yellowPieces[yellowCount][0] = i;
+					  yellowPieces[yellowCount][1] = j;
+					  yellowPieces[yellowCount][3] = board._board[i][j].king ? 1 : 0;
+					  ++yellowCount;
+					}
+					else
+					{	
+					  redCount += 1;
+					  redPieces[yellowCount][0] = i;
+					  redPieces[yellowCount][1] = j;
+					  redPieces[yellowCount][3] = board._board[i][j].king ? 1 : 0;
+					  ++redCount;
+					}
+				}
+			}
+		}
+		
+		int sumCount = redCount + redCount;
+		
+		if (sumCount >= 16) return 1;//simpleWeights(board);
+		else return 1;//simpleWeights(board);
 	}
 }
 
