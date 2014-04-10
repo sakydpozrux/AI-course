905c8f5cc5ac2cf0606a7deada0d12de044b9b9a
Szymon Koper <sakydpozrux@gmail.com>
2014-04-10 19:47:44 +0200

    Simple weights function implemented

--- 2-Checkers-heuristic/evaluateposition.java
+++ 2-Checkers-heuristic/evaluateposition.java
@@ -22,11 +22,13 @@ public class EvaluatePosition // This class is required - don't remove it
 
   static private boolean YELLOW = true;
   static private boolean RED = false;
+  static private int KING = 1;
 
 	
-  /*static int rate(int ratingYellow, int ratingRed)
-  {
+  static int rate(int ratingYellow, int ratingRed) //int ratingYellow, int ratingRed)
+  {    
 		int myRating, opponentsRating;
+		
 		if (getColor())
 		{
 		  myRating = ratingYellow;
@@ -41,36 +43,38 @@ public class EvaluatePosition // This class is required - don't remove it
 		if (myRating == 0) return LOSE; 
 		else if (opponentsRating == 0) return WIN; 
 		else return myRating-opponentsRating;
-  }*/
+  }
 
 
-	/*static int simpleWeights(AIboard board)
+  static int bestEvaluateStrategy(int[][] yellowPieces, int yellowCount, int[][] redPieces, int redCount)
+  {
+    int ratingYellow = evaluateSimpleWeights(yellowPieces, yellowCount);
+    int ratingRed = evaluateSimpleWeights(redPieces, redCount);
+
+    return rate(ratingYellow, ratingRed);
+  }
+
+
+	static int evaluateSimpleWeights(int[][] pieces, int count)
 	{
-		int ratingYellow = 0; //upper
-		int ratingRed = 0; //lower
+		int rating = 0;
 		
-		for (int i = 0; i < size; i++)
+		for (int i = 0; i < count; ++i)
 		{
-			for (int j = (i + 1) % 2; j < size; j += 2)
-			{
-				if (!board._board[i][j].empty) // field is not empty
-				{
-				  if (board._board[i][j].white) // this is upper, yellow piece
-					{
-					  if (board._board[i][j].king) ratingYellow += 25; // this is upper, yellow king
-					  else ratingYellow += 10; // this is upper, yellow piece
-					}
-					else
-					{	
-					  if (board._board[i][j].king) ratingRed += 25; // This is opponent's king
-					  else ratingRed += 10;
-					}
-				}
-			}
+		  if (pieces[i][2] == KING) rating += 25;
+		  else rating += 10;
 		}
-		
-		return rate(ratingYellow, ratingRed);
-  }	*/
+
+		return rating;
+  }
+  
+  static int maybeRotateCoordinates(int n, int size)
+  {
+    if (getColor()) // YELLOW is upper player, so array should be rotated by 180 degrees
+      return size - 1 - n;
+    else // it's lower RED turn, so coordinates are right = don't change anything
+      return n;
+  }
 
 	
 	static public int evaluatePosition(AIBoard board) // This method is required and it is the major heuristic method - type your code here
@@ -78,41 +82,37 @@ public class EvaluatePosition // This class is required - don't remove it
 		int size = board.getSize();
 		
     int yellowPieces[][] = new int [size*size][3]; // (i, j, king?)
-    int redPieces[][] = new int [size*size][3];
+    int redPieces[][] =    new int [size*size][3]; // (i, j, king?)
     
     int yellowCount = 0;
     int redCount = 0;
 		
 		for (int i = 0; i < size; i++)
-		{
 			for (int j = (i + 1) % 2; j < size; j += 2)
-			{
 				if (!board._board[i][j].empty) // field is not empty
 				{
 				  if (board._board[i][j].white) // this is upper, yellow piece
 					{
 					  yellowCount += 1;
-					  yellowPieces[yellowCount][0] = i;
-					  yellowPieces[yellowCount][1] = j;
-					  yellowPieces[yellowCount][3] = board._board[i][j].king ? 1 : 0;
+					  yellowPieces[yellowCount][0] = maybeRotateCoordinates(i, size);
+					  yellowPieces[yellowCount][1] = maybeRotateCoordinates(j, size);
+					  yellowPieces[yellowCount][2] = board._board[i][j].king ? 1 : 0;
 					  ++yellowCount;
 					}
 					else
 					{	
 					  redCount += 1;
-					  redPieces[yellowCount][0] = i;
-					  redPieces[yellowCount][1] = j;
-					  redPieces[yellowCount][3] = board._board[i][j].king ? 1 : 0;
+					  redPieces[redCount][0] = i;
+					  redPieces[redCount][1] = j;
+					  redPieces[redCount][2] = board._board[i][j].king ? 1 : 0;
 					  ++redCount;
 					}
-				}
-			}
 		}
 		
-		int sumCount = redCount + redCount;
-		
-		if (sumCount >= 16) return 1;//simpleWeights(board);
-		else return 1;//simpleWeights(board);
+		int sumPieces = yellowCount + redCount;
+
+		if (sumPieces >= 16) return bestEvaluateStrategy(yellowPieces, yellowCount, redPieces, redCount);
+		else return 1;
 	}
 }
 
