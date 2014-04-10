1092e21db0f848dc76f458132f4ef1ab9d43f7b1
Szymon Koper <sakydpozrux@gmail.com>
2014-04-10 22:30:31 +0200

    Beginning and MidGame phases finished

--- 2-Checkers-heuristic/evaluateposition.java
+++ 2-Checkers-heuristic/evaluateposition.java
@@ -35,6 +35,7 @@ public class EvaluatePosition // This class is required - don't remove it
   static private int piece_val = 10;
   static private int king_val = 19;
   static private int guard_bonus = 5;
+  static private int critical_guard_bonus = 1;
   
   static private int zone_not_border_bonus = 2;
   static private int zone_center_bonus = 1; // note that it's inside zone_not_border, so in fact it's always +3
@@ -42,6 +43,19 @@ public class EvaluatePosition // This class is required - don't remove it
   
   static private int lower_center_coordinate;
   static private int upper_center_coordinate;
+  
+  static private int almost_lower_center_coordinate;
+  static private int almost_upper_center_coordinate;
+  
+  static private int lower_agressive_coordinate; // Only for vertical coordinates
+  static private int upper_agressive_coordinate; // Only for vertical coordinates
+
+  static private int almost_lower_agressive_coordinate; // Only for vertical coordinates
+  static private int almost_upper_agressive_coordinate; // Only for vertical coordinates
+  
+  static private int critical_border_coordinate;
+  
+  static private int size;
 
 	
   static int rate(int ratingYellow, int ratingRed) //int ratingYellow, int ratingRed)
@@ -65,21 +79,8 @@ public class EvaluatePosition // This class is required - don't remove it
   }
 
 
-	static int evaluateSimpleWeights(int[][] pieces, int count)
+	static int evaluateBeginning(int[][] pieces, int count)
 	{
-		int rating = 0;
-		
-		for (int i = 0; i < count; ++i)
-		{
-		  if (pieces[i][2] == KING) rating += king_val;
-		  rating += piece_val;
-		}
-
-		return rating;
-  }
-  
-  static int evaluateMidGame(int[][] pieces, int count)
-  { //TODO
 		int rating = 0;
 		
 		int x;
@@ -88,7 +89,63 @@ public class EvaluatePosition // This class is required - don't remove it
 		{
 		  x = pieces[i][0];
 		  y = pieces[i][1];
-		  if (x)
+		  
+		  if (x >= almost_lower_center_coordinate && x < almost_upper_center_coordinate
+		     && y >= almost_lower_agressive_coordinate && y < almost_upper_agressive_coordinate)
+		  {
+		    rating += zone_not_border_bonus;
+		    if (x >= lower_center_coordinate && x < upper_center_coordinate
+		       && y >= lower_agressive_coordinate && y < upper_agressive_coordinate)
+		      rating += zone_center_bonus;
+		  }
+		  
+		  
+		  if ((x == 0 || x == size - 1) && (y == critical_border_coordinate || y == critical_border_coordinate + 1))
+		    rating += zone_strategic_field_bonus;
+		  
+		  
+		  if (y == size - 1) // guard
+		  {
+		    rating += guard_bonus;
+		    
+		    if ((x >= 2 - (size % 2)) && (x % 2 == size % 2))
+		      rating += critical_guard_bonus;
+		  }
+		  
+		  if (pieces[i][2] == KING) rating += king_val;
+		  rating += piece_val;
+		}
+
+		return rating;
+  }
+  
+  static int evaluateMidGame(int[][] pieces, int count)
+  {
+		int rating = 0;
+		
+		int x;
+		int y;
+		for (int i = 0; i < count; ++i)
+		{
+		  x = pieces[i][0];
+		  y = pieces[i][1];
+		  
+		  if (x >= almost_lower_center_coordinate && x < almost_upper_center_coordinate
+		     && y >= almost_lower_center_coordinate && y < almost_upper_center_coordinate)
+		  {
+		    rating += zone_not_border_bonus;
+		    if (x >= lower_center_coordinate && x < upper_center_coordinate
+		       && y >= lower_center_coordinate && y < upper_center_coordinate)
+		      rating += zone_center_bonus;
+		  }
+		  
+		  if (y == size - 1) // guard
+		  {
+		    rating += guard_bonus;
+		    
+		    if ((x >= 2 - (size % 2)) && (x % 2 == size % 2))
+		      rating += critical_guard_bonus;
+		  }
 		  
 		  if (pieces[i][2] == KING) rating += king_val;
 		  rating += piece_val;
@@ -147,8 +204,8 @@ public class EvaluatePosition // This class is required - don't remove it
     switch (part)
     {
       case BEGINNING:
-        ratingYellow = evaluateSimpleWeights(yellowPieces, yellowCount);
-        ratingRed    = evaluateSimpleWeights(redPieces, redCount);
+        ratingYellow = evaluateBeginning(yellowPieces, yellowCount);
+        ratingRed    = evaluateBeginning(redPieces, redCount);
         break;
       
       case MIDGAME:
@@ -179,7 +236,7 @@ public class EvaluatePosition // This class is required - don't remove it
 	
 	static public int evaluatePosition(AIBoard board) // This method is required and it is the major heuristic method - type your code here
 	{
-		int size = board.getSize();
+		size = board.getSize();
 		
     int yellowPieces[][] = new int [size*size][3]; // (i, j, king?)
     int redPieces[][] =    new int [size*size][3]; // (i, j, king?)
@@ -189,6 +246,18 @@ public class EvaluatePosition // This class is required - don't remove it
     
     lower_center_coordinate = 1 * size / 4;
     upper_center_coordinate = 3 * size / 4;
+    
+    almost_lower_center_coordinate = lower_center_coordinate - 1;
+    almost_upper_center_coordinate = upper_center_coordinate + 1;
+    
+    lower_agressive_coordinate = lower_center_coordinate - 1;
+    upper_agressive_coordinate = upper_center_coordinate - 1;
+
+    almost_lower_agressive_coordinate = lower_agressive_coordinate;
+    almost_upper_agressive_coordinate = upper_agressive_coordinate + 2;
+    
+    critical_border_coordinate = size / 2 - 1;
+		
 		
 		for (int i = 0; i < size; i++)
 			for (int j = (i + 1) % 2; j < size; j += 2)
